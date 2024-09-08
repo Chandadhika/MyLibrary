2bda3c1ed17b69e1cfb08152d13c49a9b75856f9
ANarngAung <anarngaung2-17@gmail.com>
2024-09-09 00:26:22 +0630

    added iterational html and script

---
+++ books/Front_End/M03_JavaScript/Mo3/assets/js/Funcscript.js
@@ -0,0 +1,7 @@
+// TODO: Write Your JavaScript Code Here
+var names = ["cats", "dogs", "birds", "ducks", "hens"];
+
+
+for (var i = 0; i < names.length; i++) {
+console.log(names[i]);
+}
---
+++ books/Front_End/M03_JavaScript/Mo3/assets/js/Iterscript.js
@@ -0,0 +1,15 @@
+// TODO: Write Your JavaScript Code Here
+var students = ["Siri", "Sissa", "Sumana", "Dattha", "Mano"];
+console.log(students.length);
+
+// this is for looping 
+for (var i = 0; i < students.length; i++) {
+  console.log("Welcome to our classroom" , students[i], "!");
+}
+
+// this is while loopin
+var i = 0;
+while (i < students.length){
+  console.log("Welcome to our classroom" , students[i]);
+  i++;
+}
