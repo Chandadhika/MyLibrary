733df91123a29b23889820afbd96169ed141454b
ANarngAung <anarngaung2-17@gmail.com>
2024-09-09 00:56:07 +0630

    added Functional html, script Equal Values

--- books/Front_End/M03_JavaScript/Mo3/assets/js/Funcscript.js
+++ books/Front_End/M03_JavaScript/Mo3/assets/js/Funcscript.js
@@ -1,7 +1,15 @@
 // TODO: Write Your JavaScript Code Here
-var names = ["cats", "dogs", "birds", "ducks", "hens"];
 
 
-for (var i = 0; i < names.length; i++) {
-console.log(names[i]);
-}
+var evaluateEquality = function(value1, value2) {
+    if (value1 === value2) {
+        console.log("They are equal in type and value");
+    } else if (value1 == value2) {
+        console.log("They are equal in value");
+    } else {
+        console.log("The values are not equal");
+    }
+};
+// evaluateEquality(10, 10);
+// evaluateEquality(10, "10");
+evaluateEquality(10, 'ten');
