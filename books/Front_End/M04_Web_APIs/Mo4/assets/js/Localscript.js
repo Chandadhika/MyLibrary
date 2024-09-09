ad9a53bc4433401be494b794dc0c80d67f575b65
ANarngAung <anarngaung2-17@gmail.com>
2024-09-09 22:09:19 +0630

    created Local Repostry and move oldfile

---
+++ books/Front_End/M04_Web_APIs/Mo4/assets/js/Localscript.js
@@ -0,0 +1,21 @@
+var firstNameInput = document.querySelector("#first-name");
+var lastNameInput = document.querySelector("#last-name");
+var emailInput = document.querySelector("#email");
+var passwordInput = document.querySelector("#password");
+var signUpButton = document.querySelector("#sign-up");
+
+signUpButton.addEventListener("click", function(event) {
+  event.preventDefault();
+  
+  var user = {
+    firstName: firstNameInput.value.trim(),
+    lastName: lastNameInput.value.trim(),
+    email: emailInput.value.trim(),
+    password: passwordInput.value.trim()
+  };
+
+  // set new submission to local storage 
+  localStorage.setItem("user", JSON.stringify(user));
+  // localStorage.getItem("user", JSON.parse(user)); ပြန်ခေါ်သုံးတာ
+  // localStorage.removeItem("user", JSON.stringify(user)); ဖျက်ပစ်တာ
+});
