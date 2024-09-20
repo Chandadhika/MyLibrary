18c5a72540bbc00e2d35ee7a7c94d944a169158b
ANarngAung <anarngaung2-17@gmail.com>
2024-09-20 14:41:10 +0630

    changed name and add create-fetch file

---
+++ books/Front_End/M06_Server_Side_APIs/M06/assets/js/Createscript.js
@@ -0,0 +1,55 @@
+/*var repoList = document.querySelector('ul');
+var fetchButton = document.getElementById('fetch-button');
+
+// `getApi` function is called when the `fetchButton` is clicked
+
+function getApi() {
+  // TODO: Insert the API url to get a list of your repos
+  var requestUrl = '';
+
+  fetch(requestUrl)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      // Looping over the fetch response and inserting the URL of your repos into a list
+      for (var i = 0; i < data.length; i++) {
+        // Create a list element
+        var listItem = document.createElement('li');
+
+        // Set the text of the list element to the JSON response's .html_url property
+        listItem.textContent = data[i].html_url;
+
+        // Append the li element to the id associated with the ul element.
+        repoList.appendChild(listItem);
+      }
+    });
+}
+
+fetchButton.addEventListener('click', getApi);*/
+
+var repoList = document.querySelector('ul');
+var fetchButton = document.getElementById('fetch-button');
+
+function getApi() {
+  // Replace `octocat` with anyone else's GitHub username
+  var requestUrl = 'https://api.github.com/users/Chandadhika/repos';
+
+  fetch(requestUrl)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      for (var i = 0; i < data.length; i++) {
+        var listItem = document.createElement('li');
+
+        listItem.textContent = data[i].html_url;
+
+        repoList.appendChild(listItem);
+      }
+    });
+}
+
+fetchButton.addEventListener('click', getApi);
+//Repos = [1, 2, 3, 4, 5, 6]
+
