7006fdd0fd5d8faa5d0d1d1ac36a3986621409f5
ANarngAung <anarngaung2-17@gmail.com>
2024-09-21 03:50:17 +0630

    added arithmetic, log-nums and odd-or-even

---
+++ books/Front_End/M03_JavaScript/Mo3/assets/js/algorithms/odd-or-even.js
@@ -0,0 +1,12 @@
+// Write code to return the string "odd" if `num` is an odd number
+// and return the string "even" if `num` is an even number
+
+// var oddOrEven = function(num) {};
+var num = [1, 2, 3, 4, 5, 6, 7, 8]
+var oddOrEven = function(num) {
+    if (num % 2 === 0) {
+        return "even";
+    } else {
+        return "odd";
+    }
+}; 
