8e7de1aae0dee6a5e51bfc3137e9d237656aa595
ANarngAung <anarngaung2-17@gmail.com>
2024-09-21 01:07:15 +0630

    added arithmetic files

---
+++ books/Front_End/M03_JavaScript/Mo3/assets/js/arithmetic.js
@@ -0,0 +1,21 @@
+// Fill in the body of each function with the code required
+
+var add = function(num1, num2) {
+    return (num1 + num2);
+};
+add(30, 40);
+
+var subtract = function(num1, num2) {
+    return (num1 - num2);
+};
+subtract(30, 20);
+
+var multiply = function(num1, num2) {
+    return (num1 * num2);
+};
+multiply(2, 8);
+
+var divide = function(num1, num2) {
+    return (num1 / num2);
+};
+divide(9, 3);
